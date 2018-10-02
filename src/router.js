b76f0c7864b1911eef0b72a68087c9581f8999d0
voxace <dave@voxace.com>
2018-10-02 22:23:29 +1000

    Default route

--- src/router.js
+++ src/router.js
@@ -1,42 +1,51 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/about",
+      name: "about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
-      path: '/nutrition',
-      name: 'nutrition',
-      component: () => import(/* webpackChunkName: "about" */ './views/Nutrition.vue')
+      path: "/nutrition",
+      name: "nutrition",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Nutrition.vue")
     },
     {
-      path: '/personal-training',
-      name: 'personal-training',
-      component: () => import(/* webpackChunkName: "about" */ './views/PersonalTraining.vue')
+      path: "/personal-training",
+      name: "personal-training",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/PersonalTraining.vue")
     },
     {
-      path: '/prices',
-      name: 'prices',
-      component: () => import(/* webpackChunkName: "about" */ './views/Prices.vue')
+      path: "/prices",
+      name: "prices",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Prices.vue")
     },
     {
-      path: '/contact',
-      name: 'contact',
-      component: () => import(/* webpackChunkName: "about" */ './views/Contact.vue')
+      path: "/contact",
+      name: "contact",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Contact.vue")
+    },
+    {
+      path: "*",
+      redirect: "/"
     }
   ]
-})
+});
