12851142a9ce22c18731da203ee760561f787d1e
David Steedman <dave@voxace.com>
2020-01-20 10:18:04 +1100

    Latest update

--- src/components/home/Body.vue
+++ src/components/home/Body.vue
@@ -3,19 +3,35 @@
     <div id="fb-root"></div>
     <section>
       <v-container grid-list-xl>
-        <v-layout row wrap justify-center>
-          <v-flex xs12 xl9 lg8 md8 sm6>
-            <v-layout row wrap>
+        <v-layout
+          row
+          wrap
+          justify-center
+        >
+          <v-flex
+            xs12
+            xl9
+            lg8
+            md8
+            sm6
+          >
+            <v-layout
+              row
+              wrap
+            >
               <v-flex xs12>
                 <v-card class="elevation-0 transparent">
                   <h1 class="display-2">Fit and Fueled</h1>
                   <h2 class="display-1 my-3">Real Food. Effective Fitness.</h2>
                   <h3 class="title font-weight-light my-3">Accredited Practising Dietitian and Certified Personal Trainer specialising
-                    in whole food nutrition for weight loss, well-being and sports performance.<h3>
+                    in whole food nutrition for weight loss, well-being and sports performance.</h3>
                 </v-card>
               </v-flex>
 
-              <v-flex md5 sm12>
+              <v-flex
+                md5
+                sm12
+              >
                 <v-card class="elevation-0 transparent">
                   <h1 class="headline">Nutrition</h1>
                   <p class="text-xs-center body-1 my-1">Whole Food / Paleo</p>
@@ -23,17 +39,28 @@
                   <p class="text-xs-center body-1 my-1">General Health & Wellbeing</p>
                   <p class="text-xs-center body-1 my-1">Special Dietary Conditions</p>
                   <p class="text-xs-center body-1 my-1">Weight loss</p>
-                  </ul>
-                  <v-btn color="success" large class="my-3" @click="findOutMoreNutrition()">Find out more!</v-btn>
+                  <v-btn
+                    color="success"
+                    large
+                    class="my-3"
+                    @click="findOutMoreNutrition()"
+                  >Find out more!</v-btn>
                 </v-card>
 
               </v-flex>
 
-              <v-flex md2 sm12 class="hidden-sm-and-down">
+              <v-flex
+                md2
+                sm12
+                class="hidden-sm-and-down"
+              >
                 <v-divider vertical></v-divider>
               </v-flex>
 
-              <v-flex md5 sm12>
+              <v-flex
+                md5
+                sm12
+              >
                 <v-card class="elevation-0 transparent">
                   <h1 class="headline">Personal Training</h1>
                   <p class="text-xs-center body-1 my-1">Individuals</p>
@@ -41,15 +68,28 @@
                   <p class="text-xs-center body-1 my-1">Weight management</p>
                   <p class="text-xs-center body-1 my-1">Functional Fitness</p>
                   <p class="text-xs-center body-1 my-1">Strength Training</p>
-                  <v-btn color="success" large class="my-3" @click="findOutMorePT()">Find out more!</v-btn>
+                  <v-btn
+                    color="success"
+                    large
+                    class="my-3"
+                    @click="findOutMorePT()"
+                  >Find out more!</v-btn>
                 </v-card>
               </v-flex>
 
             </v-layout>
 
-            <v-layout row wrap justify-center class="hidden-sm-only">
+            <v-layout
+              row
+              wrap
+              justify-center
+              class="hidden-sm-only"
+            >
               <v-flex xs12>
-                <img :src="associations" class="responsive"/>
+                <img
+                  :src="associations"
+                  class="responsive"
+                />
               </v-flex>
               <v-flex xs12>
                 <h2 class="display-1">Health fund rebates available!</h2>
@@ -58,9 +98,13 @@
 
           </v-flex>
 
-
-
-          <v-flex xs12 xl3 lg4 md4 sm6>
+          <v-flex
+            xs12
+            xl3
+            lg4
+            md4
+            sm6
+          >
             <div
               class="fb-page"
               data-href="https://www.facebook.com/fitandfueled.com.au"
@@ -69,9 +113,12 @@
               data-adapt-container-width="true"
               data-height="700px"
               data-hide-cover="true"
-              data-show-facepile="true">
-              <blockquote cite="https://www.facebook.com/fitandfueled.com.au"
-                class="fb-xfbml-parse-ignore">
+              data-show-facepile="true"
+            >
+              <blockquote
+                cite="https://www.facebook.com/fitandfueled.com.au"
+                class="fb-xfbml-parse-ignore"
+              >
                 <a href="https://www.facebook.com/fitandfueled.com.au">Fit and Fueled</a>
               </blockquote>
             </div>
@@ -79,9 +126,17 @@
 
         </v-layout>
 
-        <v-layout row wrap justify-center class="hidden-xs-only hidden-md-and-up">
+        <v-layout
+          row
+          wrap
+          justify-center
+          class="hidden-xs-only hidden-md-and-up"
+        >
           <v-flex xs12>
-            <img :src="associations" class="responsive"/>
+            <img
+              :src="associations"
+              class="responsive"
+            />
           </v-flex>
           <v-flex xs12>
             <h2 class="display-1">Health fund rebates available!</h2>
@@ -95,37 +150,37 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        associations: require('@/assets/associations.jpg')
-      }
+export default {
+  data () {
+    return {
+      associations: require('@/assets/associations.jpg')
+    }
+  },
+  created () {
+    if (!fbsdk) {
+      var fbsdk = (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.1';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    }
+  },
+  methods: {
+    findOutMoreNutrition () {
+      this.$router.push('nutrition');
     },
-    created() {
-      if(!fbsdk) {
-        var fbsdk = (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.1';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-      }
-    },
-    methods: {
-      findOutMoreNutrition() {
-        this.$router.push('nutrition');
-      },
-      findOutMorePT() {
-        this.$router.push('personal-training');
-      }
+    findOutMorePT () {
+      this.$router.push('personal-training');
     }
   }
+}
 </script>
 
 <style>
-  .responsive {
-    height: auto;
-    max-width: 100%;
-  }
+.responsive {
+  height: auto;
+  max-width: 100%;
+}
 </style>
--- src/views/Prices.vue
+++ src/views/Prices.vue
@@ -1,9 +1,16 @@
 <template>
   <v-container grid-list-xl>
     <h1 class="display-2 text-xs-center">Prices</h1>
-    <v-layout row wrap class="mt-3">
+    <v-layout
+      row
+      wrap
+      class="mt-3"
+    >
 
-      <v-flex xs12 md6>
+      <v-flex
+        xs12
+        md6
+      >
         <h3 class="headline mb-3 text-xs-center">
           Nutrition
         </h3>
@@ -13,14 +20,20 @@
           hide-actions
           class="elevation-1"
         >
-          <template slot="items" slot-scope="props">
+          <template
+            slot="items"
+            slot-scope="props"
+          >
             <td class="text-xs-left">{{ props.item.description }}</td>
             <td class="text-xs-right">{{ props.item.price }}</td>
           </template>
         </v-data-table>
       </v-flex>
 
-      <v-flex xs12 md6>
+      <v-flex
+        xs12
+        md6
+      >
         <h3 class="headline mb-3 text-xs-center">
           Personal Training
         </h3>
@@ -30,10 +43,12 @@
           hide-actions
           class="elevation-1"
         >
-          <template slot="items" slot-scope="props">
+          <template
+            slot="items"
+            slot-scope="props"
+          >
             <td class="text-xs-left">{{ props.item.description }}</td>
             <td class="text-xs-right">{{ props.item.single }}</td>
-            <td class="text-xs-right">{{ props.item.pack }}</td>
           </template>
         </v-data-table>
       </v-flex>
@@ -50,9 +65,15 @@
         </p>
       </v-flex>
 
-      <v-flex xs12 md4>
+      <v-flex
+        xs12
+        md4
+      >
         <v-card hover>
-          <v-card-title primary-title class="justify-center light-green accent-4">
+          <v-card-title
+            primary-title
+            class="justify-center light-green accent-4"
+          >
             <div>
               <h3 class="headline mb-1">Basic</h3>
               <div>LET'S GET STARTED</div>
@@ -61,23 +82,32 @@
           <v-card-text class="justify-center">
             <div>
               <h1 class="display-2 mb-1">$199</h1>
-              <h4>SAVE $80</h4>
-              <p class="body-1 my-1 text-xs-center">3 day dietary analysis</p>
+              <h4>SAVE $30</h4>
               <p class="body-1 my-1 text-xs-center">Initial dietary consult</p>
               <p class="body-1 my-1 text-xs-center">Initial 1 hour PT consult</p>
               <p class="body-1 my-1 text-xs-center"> - </p>
-              <p class="body-1 my-1 text-xs-center">1 Additional 45 min PT session</p>
+              <p class="body-1 my-1 text-xs-center">+ 1 Additional dietary review</p>
             </div>
           </v-card-text>
           <v-card-actions class="justify-center">
-            <v-btn flat color="light-green accent-4" @click="findOutMore()">Find out more</v-btn>
+            <v-btn
+              flat
+              color="light-green accent-4"
+              @click="findOutMore()"
+            >Find out more</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
 
-      <v-flex xs12 md4>
+      <v-flex
+        xs12
+        md4
+      >
         <v-card hover>
-          <v-card-title primary-title class="justify-center light-green accent-4">
+          <v-card-title
+            primary-title
+            class="justify-center light-green accent-4"
+          >
             <div>
               <h3 class="headline mb-1">Standard</h3>
               <div>4 WEEK MOTIVATOR</div>
@@ -85,24 +115,33 @@
           </v-card-title>
           <v-card-text class="justify-center">
             <div>
-              <h1 class="display-2 mb-1">$329</h1>
-              <h4>SAVE $85</h4>
-              <p class="body-1 my-1 text-xs-center">3 day dietary analysis</p>
+              <h1 class="display-2 mb-1">$319</h1>
+              <h4>SAVE $50</h4>
               <p class="body-1 my-1 text-xs-center">Initial dietary consult</p>
               <p class="body-1 my-1 text-xs-center">Initial 1 hour PT consult</p>
               <p class="body-1 my-1 text-xs-center">1 30 min dietary follow-up session</p>
-              <p class="body-1 my-1 text-xs-center">3 Additional 45 min PT sessions</p>
+              <p class="body-1 my-1 text-xs-center">+ 1 Additional 45 min PT session</p>
             </div>
           </v-card-text>
           <v-card-actions class="justify-center">
-            <v-btn flat color="light-green accent-4" @click="findOutMore()">Find out more</v-btn>
+            <v-btn
+              flat
+              color="light-green accent-4"
+              @click="findOutMore()"
+            >Find out more</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
 
-      <v-flex xs12 md4>
+      <v-flex
+        xs12
+        md4
+      >
         <v-card hover>
-          <v-card-title primary-title class="justify-center light-green accent-4">
+          <v-card-title
+            primary-title
+            class="justify-center light-green accent-4"
+          >
             <div>
               <h3 class="headline mb-1">Premium</h3>
               <div>12 WEEK COACH</div>
@@ -110,17 +149,20 @@
           </v-card-title>
           <v-card-text class="justify-center">
             <div>
-              <h1 class="display-2 mb-1">$699</h1>
-              <h4>SAVE $200</h4>
-              <p class="body-1 my-1 text-xs-center">3 day dietary analysis</p>
+              <h1 class="display-2 mb-1">$799</h1>
+              <h4>SAVE $100</h4>
               <p class="body-1 my-1 text-xs-center">Initial dietary consult</p>
               <p class="body-1 my-1 text-xs-center">Initial 1 hour PT consult</p>
-              <p class="body-1 my-1 text-xs-center">4 30 min dietary follow-up sessions</p>
-              <p class="body-1 my-1 text-xs-center">11 Additional 45 min PT sessions</p>
+              <p class="body-1 my-1 text-xs-center">4 x 30 min dietary follow-up sessions</p>
+              <p class="body-1 my-1 text-xs-center">+ 1 Additional 45 min PT session</p>
             </div>
           </v-card-text>
           <v-card-actions class="justify-center">
-            <v-btn flat color="light-green accent-4" @click="findOutMore()">Find out more</v-btn>
+            <v-btn
+              flat
+              color="light-green accent-4"
+              @click="findOutMore()"
+            >Find out more</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -130,98 +172,93 @@
 </template>
 
 <script>
-  export default {
-    name: 'prices',
-    data () {
-      return {
-        headers1: [
-          { text: 'Description', align: 'left', sortable: false, value: 'description' },
-          { text: 'Price', align: 'right', sortable: false, value: 'price' }
-        ],
-        data1: [
-          {
-            value: false,
-            description: 'Initial Dietetic Consult (1 hour)',
-            price: '$80'
-          },
-          {
-            value: false,
-            description: 'Dietetic Review (30 min)',
-            price: '$40'
-          },
-          {
-            value: false,
-            description: 'Dietetic Review (5 pack)',
-            price: '$175'
-          },
-          {
-            value: false,
-            description: '1 Week Custom Meal Plan (includes initial consult)',
-            price: '$150'
-          },
-          {
-            value: false,
-            description: 'Nutrition Overhaul (includes initial consult, dietary analysis, 1 week custom meal plan and one month unlimited nutrition support via telephone/email)',
-            price: '$400'
-          },
-        ],
-        headers2: [
-          { text: 'Description', align: 'left', sortable: false, value: 'description' },
-          { text: 'Single ($)', align: 'right', sortable: false, value: 'single' },
-          { text: '10 Pack* ($)', align: 'right', sortable: false, value: 'pack' }
-        ],
-        data2: [
-          {
-            value: false,
-            description: '45 min PT session (1-on-1)',
-            single: '$45',
-            pack: '$400'
-          },
-          {
-            value: false,
-            description: '60 min PT session (1-on-1)',
-            single: '$55',
-            pack: '$500'
-          },
-          {
-            value: false,
-            description: '45 min PT session (2-on-1)',
-            single: '$25 each',
-            pack: '$240 each'
-          },
-          {
-            value: false,
-            description: '60 min PT session (2-on-1)',
-            single: '$30 each',
-            pack: '$300 each'
-          },
-          {
-            value: false,
-            description: '45 min PT session (3-4 people)',
-            single: '$20 each',
-            pack: '$165 each'
-          },
-          {
-            value: false,
-            description: '60 min PT session (3-4 people)',
-            single: '$25 each',
-            pack: '$210 each'
-          }
-        ]
-      }
-    },
-    methods: {
-      findOutMore() {
-        this.$router.push('contact');
-      }
+export default {
+  name: 'prices',
+  data () {
+    return {
+      headers1: [
+        { text: 'Description', align: 'left', sortable: false, value: 'description' },
+        { text: 'Price', align: 'right', sortable: false, value: 'price' }
+      ],
+      data1: [
+        {
+          value: false,
+          description: 'Initial Dietetic Consult (1 hour)',
+          price: '$80'
+        },
+        {
+          value: false,
+          description: 'Dietetic Review (30 min)',
+          price: '$40'
+        },
+        {
+          value: false,
+          description: 'Dietetic Review (5 pack)',
+          price: '$175'
+        },
+        {
+          value: false,
+          description: '1 Week Custom Meal Plan (includes initial consult)',
+          price: '$150'
+        },
+        {
+          value: false,
+          description: 'Nutrition Overhaul (includes initial consult, dietary analysis, 1 week custom meal plan and one month unlimited nutrition support via telephone/email)',
+          price: '$400'
+        },
+      ],
+      headers2: [
+        { text: 'Description', align: 'left', sortable: false, value: 'description' },
+        { text: 'Price ($)', align: 'right', sortable: false, value: 'single' }
+      ],
+      data2: [
+        {
+          value: false,
+          description: '45 min PT session (1-on-1)',
+          single: '$50'
+        },
+        {
+          value: false,
+          description: '60 min PT session (1-on-1)',
+          single: '$60'
+        },
+        {
+          value: false,
+          description: '45 min PT session (2-on-1)',
+          single: '$30 each'
+        },
+        {
+          value: false,
+          description: '60 min PT session (2-on-1)',
+          single: '$35 each'
+        },
+        {
+          value: false,
+          description: '45 min PT session (3-on-1)',
+          single: '$20 each'
+        },
+        {
+          value: false,
+          description: '60 min PT session (3-on-1)',
+          single: '$25 each'
+        }
+      ]
+    }
+  },
+  methods: {
+    findOutMore () {
+      this.$router.push('contact');
     }
   }
+}
 </script>
 
 <style>
-  thead, th, thead tr {
-    background-color: #64dd17 !important;
-    color: black !important;
-    font-size: 16px !important;
-  }
+thead,
+th,
+thead tr {
+  background-color: #64dd17 !important;
+  color: black !important;
+  font-size: 16px !important;
+}
 </style>
